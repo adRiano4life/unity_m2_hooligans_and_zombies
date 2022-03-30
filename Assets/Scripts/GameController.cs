05fb6689b1b0e18897a6227d3f602ffb1aa4751a
Alexandr Folomeshkin <fifax.adriano@gmail.com>
2022-03-30 13:42:40 +0600

    Change Canvas

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,9 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
-using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
